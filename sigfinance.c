71f23f0917c76a854323d6fd48d8eaac20d173f1
Tomé Galileu Oliveira Arcanjo <tome.arcanjo.112@ufrn.edu.br>
2024-10-13 23:05:52 -0300

    criando do while do menu tarefas

--- sigfinance.c
+++ sigfinance.c
@@ -334,45 +334,46 @@ void excluir_despesa()
     getchar();  // Aguarda o usuário pressionar Enter
 }
 
-void menu_tarefas()
-{
-    system("clear||cls");
+void menu_tarefas(){
     int opcao;
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///            = = = = = = = = = Menu Tarefas = = = = = = = = =             ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Cadastrar uma tarefa                                      ///\n");
-    printf("///            2. Pesquisar as tarefas                                      ///\n");
-    printf("///            3. Atualizar tarefas                                         ///\n");
-    printf("///            4. Excluir uma tarefa                                        ///\n");
-    printf("///            0. Voltar ao menu anterior                                   ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Escolha a opção desejada: ");
-    scanf("%d", &opcao);
-    getchar();
-    switch (opcao)
-    {
-    case 1:
-        cadastrar_tarefa();
-        break;
-    case 2:
-        pesquisar_tarefa();
-        break;
-    case 3:
-        atualizar_tarefa();
-        break;
-    case 4:
-        excluir_tarefa();
-        break;
-    case 0:
-        tela_principal();
-        break;
-    default:
-        printf("Opção inválida! Tente novamente.\n");
-        menu_tarefas();
-        break;
-    }
+    do{   
+        system("clear||cls");
+        printf("\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///            = = = = = = = = = Menu Tarefas = = = = = = = = =             ///\n");
+        printf("///                                                                         ///\n");
+        printf("///            1. Cadastrar uma tarefa                                      ///\n");
+        printf("///            2. Pesquisar as tarefas                                      ///\n");
+        printf("///            3. Atualizar tarefas                                         ///\n");
+        printf("///            4. Excluir uma tarefa                                        ///\n");
+        printf("///            0. Voltar ao menu anterior                                   ///\n");
+        printf("///                                                                         ///\n");
+        printf("///            Escolha a opção desejada: ");
+        scanf("%d", &opcao);
+        getchar();
+        switch (opcao)
+        {
+        case 1:
+            cadastrar_tarefa();
+            break;
+        case 2:
+            pesquisar_tarefa();
+            break;
+        case 3:
+            atualizar_tarefa();
+            break;
+        case 4:
+            excluir_tarefa();
+            break;
+        case 0:
+            tela_principal();
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+            menu_tarefas();
+            break;
+        }
+    } while (opcao != 0);
 }
 
 void cadastrar_tarefa()
