2487dcb34d43bed516d3dc20641476811392cede
coderlupus <nathanpeadr922@gmail.com>
2024-10-13 22:26:26 -0300

    Colocando do while na tela principal

--- sigfinance.c
+++ sigfinance.c
@@ -38,60 +38,61 @@ int main(void)
 }
 
 /*FUNÇÕES/FUNCTIONS*/
-void tela_principal(void)
-{
+void tela_principal(void){
     int opcao;
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///      Um sistema de Gestão de Despesas de uma República Estudantil        ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             = = = = Sistema de Gestão De Despesas = = = =               ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Módulo Moradores                                          ///\n");
-    printf("///            2. Módulo Despesas                                           ///\n");
-    printf("///            3. Módulo Tarefas diárias                                    ///\n");
-    printf("///            4. Módulo Relatórios                                         ///\n");
-    printf("///            5. Módulo Informações                                        ///\n");
-    printf("///            0. Sair                                                      ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Escolha a opção desejada: ");
-    scanf("%d", &opcao);
-    getchar();
-    switch (opcao)
-    {
-    case 1:
-        menu_moradores();
-        break;
-    case 2:
-        menu_despesas();
-        break;
-    case 3:
-        menu_tarefas(); 
-        break;
-    case 4:
-        menu_relatorios(); 
-        break;
-    case 5:
-        menu_informacoes();
-        break;
-    case 0:
-        printf("Saindo do sistema...\n");
-        exit(0);
-    default:
-        printf("Opção inválida! Tente novamente.\n");
-        tela_principal();
-        break;
-    }
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>Tecle <ENTER> para continuar...\n");
+    do{
+        system("clear||cls");
+        printf("\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                         ///\n");
+        printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+        printf("///      Um sistema de Gestão de Despesas de uma República Estudantil        ///\n");
+        printf("///                                                                         ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                         ///\n");
+        printf("///             = = = = Sistema de Gestão De Despesas = = = =               ///\n");
+        printf("///                                                                         ///\n");
+        printf("///            1. Módulo Moradores                                          ///\n");
+        printf("///            2. Módulo Despesas                                           ///\n");
+        printf("///            3. Módulo Tarefas diárias                                    ///\n");
+        printf("///            4. Módulo Relatórios                                         ///\n");
+        printf("///            5. Módulo Informações                                        ///\n");
+        printf("///            0. Sair                                                      ///\n");
+        printf("///                                                                         ///\n");
+        printf("///            Escolha a opção desejada: ");
+        scanf("%d", &opcao);
+        getchar();
+        
+        switch (opcao){
+        case 1:
+            menu_moradores();
+            break;
+        case 2:
+            menu_despesas();
+            break;
+        case 3:
+            menu_tarefas();
+            break;
+        case 4:
+            menu_relatorios();
+            break;
+        case 5:
+            menu_informacoes();
+            break;
+        case 0:
+            printf("Saindo do sistema...\n");
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+        }
+        printf("///                                                                         ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        printf("\t\t\t>Tecle <ENTER> para continuar...\n");
+        getchar();
+    } while (opcao != 0);
 }
+
 
 void menu_moradores()
 {
