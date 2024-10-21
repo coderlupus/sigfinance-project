1977b4190b443f3d2244da1093921d78e690d0b4
coderlupus <nathanpeadr922@gmail.com>
2024-10-21 09:49:29 -0300

    adicionando modularização do modulo informações

--- sigfinance.c
+++ sigfinance.c
@@ -5,11 +5,10 @@
 #include "despesas.h"
 #include "tarefas.h"
 #include "relatorios.h"
+#include "informacoes.h"
 
 /*ASSINATURA DAS FUNÇÕES/FUNCTION'S SIGNATURE*/
 void tela_principal(void); /* void: Significa que a função não retorna nenhum valor e (void): Indica que a função não recebe nenhum parâmetro.*/
-
-void menu_informacoes(void);
 
 /*FUNÇÃO MAIN/MAIN FUNCTION:*/
 int main(void)
@@ -19,9 +18,11 @@ int main(void)
 }
 
 /*FUNÇÕES/FUNCTIONS*/
-void tela_principal(void){
+void tela_principal(void)
+{
     int opcao;
-    do{
+    do
+    {
         system("clear||cls");
         printf("\n");
         printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -43,8 +44,9 @@ void tela_principal(void){
         printf("///            Escolha a opção desejada: ");
         scanf("%d", &opcao);
         getchar();
-        
-        switch (opcao){
+
+        switch (opcao)
+        {
         case 1:
             menu_moradores();
             break;
@@ -72,32 +74,4 @@ void tela_principal(void){
             getchar();
         }
     } while (opcao != 0);
-}
-
-
-void menu_informacoes(void)
-{
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///      Um sistema de Gestão de Despesas de uma República Estudantil       ///\n");
-    printf("///Esse programa tem o intuito de facilitar o manejamento das despesas para ///\n");
-    printf("///um grupo de moradores de uma república de estudantes, incluindo 3 módulos///\n");
-    printf("///principais: Moradores, Despesas e Tarefas Diárias. Esses módulos permitem///\n");
-    printf("///a fácil administração das responsabilidades de cada morador da república.///\n");
-    printf("///                  - - - Equipe de desenvolvimento - - -                  ///\n");
-    printf("///   - - - Tomé Galileu Oliveira Arcanjo e Nathan Lopes Rodrigues - - -    ///\n");
-    printf("///                  - - -         Instagram      - - -                     ///\n");
-    printf("///   - - - @Tome.arcanjo              e          @nlpsr_          - - -    ///\n");
-    printf("///                  - - -           Github       - - -                     ///\n");
-    printf("///   - - - @tome-arcanjo              e          @coderlupus      - - -    ///\n");
-    printf("///                  - - -           E-mail       - - -                     ///\n");
-    printf("///   - - - tomearcanjo12@gmail.com    e     nlopesr2006@gmail.com - - -    ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-
-    printf("\n\t\t\t>>> Tecle <ENTER> para voltar ao menu principal...\n");
-    getchar();        // Espera o usuário apertar "Enter"
-    // tela_principal(); // Volta ao menu principal
 }
